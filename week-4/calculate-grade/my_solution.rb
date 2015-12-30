a560215f015a9e5036d1146b954029eaaebc61b9
Mollie Stein <mastein109@gmail.com>
2015-12-29 19:05:20 -0500

    Add grade average spec and solution

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,19 @@
+# Calculate a Grade
+
+# I worked on this challenge [by myself ].
+
+
+# Your Solution Below
+def get_grade(i)
+  if i >= 90
+    return "A"
+  elsif i >= 80
+    return "B"
+  elsif i >= 70
+    return "C"
+  elsif i >= 60
+    return "D"
+  else
+    return "F"
+  end
+end
