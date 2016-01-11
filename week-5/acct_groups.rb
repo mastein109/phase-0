532472f78b944bfcf2b6288603cf3d39ea863d48
Mollie Stein <mastein109@gmail.com>
2016-01-11 12:17:21 -0600

    Add fiddler crabs list

--- week-5/acct_groups.rb
+++ week-5/acct_groups.rb
@@ -36,6 +36,67 @@ end
   puts group_hash
 end
 
+#Driver Code
+
+fiddler_crabs = ["Aaron Hu",
+"Adam Pinsky",
+"Akeem Street",
+"Alex Forger",
+"Andrew Kim",
+"Baron Kwan",
+"Brian Bier",
+"Byron Gage",
+"Carl Conroy",
+"Charlie Bliss",
+"Christopher Bunkers",
+"Cody Kendall",
+"Coline Forde",
+"David Valencia",
+"Emily Bosakowski",
+"Everett Golden",
+"Hagai Zwick",
+"Heather C",
+"Ian Wudarski",
+"Ieronim Oltean",
+"Jake Hamilton",
+"James Boyd",
+"Jasmeet Chatrath",
+"Jenna El-Amin",
+"Jerrie Evans",
+"Joe Case",
+"Jonathan Case",
+"Jonathan Schwartz",
+"Jonathan Silvestri",
+"Kathryn Garbacz",
+"Ian Kinner",
+"Kyle Cierzan",
+"Kyle Zelman",
+"Andrew Larson",
+"Linda Oanh Ho",
+"Yiorgos Makridakis",
+"Matt Harris",
+"Matthew Baquerizo",
+"Menuka Samaranayake",
+"Michael Pintar",
+"Mollie Stein",
+"Lydia Nash",
+"Aaron Opsahl",
+"Peter Leong",
+"Peter Stratoudakis",
+"Prince Sadie",
+"Ryan F. Salerno",
+"Sanderfer Chau",
+"Sarah Finken",
+"EMMET SUSSLIN",
+"Sydney Rossman-Reich",
+"Eric Tenza",
+"Thomas Yancey",
+"Tim Kelly",
+"Timothy Beck",
+"Tyler Doerschuk",]
+
+p acc_group(fiddler_crabs)
+
 =begin
 What was the most interesting and most difficult part of this challenge?
 The most difficult part of this challenge was having the method divide not only the list into groups, but make sure the groups include at least 3 people.
