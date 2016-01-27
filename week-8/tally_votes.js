04c1a36caec69b8996568f6c877fb96cf2fa47b5
Mollie Stein <mastein109@gmail.com>
2016-01-26 20:32:05 -0600

    Add 8.3 solution and reflection

---
+++ week-8/tally_votes.js
@@ -0,0 +1,191 @@
+// Tally Votes in JavaScript Pairing Challenge.
+
+// I worked on this challenge with: Sydney Rossman-Reich
+// This challenge took me [1.25] hours.
+
+// These are the votes cast by each student. Do not alter these objects here.
+var votes = {
+  "Alex": { president: "Bob", vicePresident: "Devin", secretary: "Gail", treasurer: "Kerry" },
+  "Bob": { president: "Mary", vicePresident: "Hermann", secretary: "Fred", treasurer: "Ivy" },
+  "Cindy": { president: "Cindy", vicePresident: "Hermann", secretary: "Bob", treasurer: "Bob" },
+  "Devin": { president: "Louise", vicePresident: "John", secretary: "Bob", treasurer: "Fred" },
+  "Ernest": { president: "Fred", vicePresident: "Hermann", secretary: "Fred", treasurer: "Ivy" },
+  "Fred": { president: "Louise", vicePresident: "Alex", secretary: "Ivy", treasurer: "Ivy" },
+  "Gail": { president: "Fred", vicePresident: "Alex", secretary: "Ivy", treasurer: "Bob" },
+  "Hermann": { president: "Ivy", vicePresident: "Kerry", secretary: "Fred", treasurer: "Ivy" },
+  "Ivy": { president: "Louise", vicePresident: "Hermann", secretary: "Fred", treasurer: "Gail" },
+  "John": { president: "Louise", vicePresident: "Hermann", secretary: "Fred", treasurer: "Kerry" },
+  "Kerry": { president: "Fred", vicePresident: "Mary", secretary: "Fred", treasurer: "Ivy" },
+  "Louise": { president: "Nate", vicePresident: "Alex", secretary: "Mary", treasurer: "Ivy" },
+  "Mary": { president: "Louise", vicePresident: "Oscar", secretary: "Nate", treasurer: "Ivy" },
+  "Nate": { president: "Oscar", vicePresident: "Hermann", secretary: "Fred", treasurer: "Tracy" },
+  "Oscar": { president: "Paulina", vicePresident: "Nate", secretary: "Fred", treasurer: "Ivy" },
+  "Paulina": { president: "Louise", vicePresident: "Bob", secretary: "Devin", treasurer: "Ivy" },
+  "Quintin": { president: "Fred", vicePresident: "Hermann", secretary: "Fred", treasurer: "Bob" },
+  "Romanda": { president: "Louise", vicePresident: "Steve", secretary: "Fred", treasurer: "Ivy" },
+  "Steve": { president: "Tracy", vicePresident: "Kerry", secretary: "Oscar", treasurer: "Xavier" },
+  "Tracy": { president: "Louise", vicePresident: "Hermann", secretary: "Fred", treasurer: "Ivy" },
+  "Ullyses": { president: "Louise", vicePresident: "Hermann", secretary: "Ivy", treasurer: "Bob" },
+  "Valorie": { president: "Wesley", vicePresident: "Bob", secretary: "Alex", treasurer: "Ivy" },
+  "Wesley": { president: "Bob", vicePresident: "Yvonne", secretary: "Valorie", treasurer: "Ivy" },
+  "Xavier": { president: "Steve", vicePresident: "Hermann", secretary: "Fred", treasurer: "Ivy" },
+  "Yvonne": { president: "Bob", vicePresident: "Zane", secretary: "Fred", treasurer: "Hermann" },
+  "Zane": { president: "Louise", vicePresident: "Hermann", secretary: "Fred", treasurer: "Mary" }
+}
+
+// Tally the votes in voteCount.
+var voteCount = {
+  president: {},
+  vicePresident: {},
+  secretary: {},
+  treasurer: {}
+}
+
+/* The name of each student receiving a vote for an office should become a property
+of the respective office in voteCount.  After Alex's votes have been tallied,
+voteCount would be ...
+
+  var voteCount = {
+    president: { Bob: 1 },
+    vicePresident: { Devin: 1 },
+    secretary: { Gail: 1 },
+    treasurer: { Kerry: 1 }
+  }
+
+*/
+
+
+/* Once the votes have been tallied, assign each officer position the name of the
+student who received the most votes. */
+var officers = {
+  president: undefined,
+  vicePresident: undefined,
+  secretary: undefined,
+  treasurer: undefined
+}
+
+// Pseudocode
+// 1. iterate through votes and pull names by position
+// 2. iterate further through position and pull candidate names
+// 3. count how many times each candidate name appeared per position
+// 4. record number of votes for each candidate for each position in new object
+// 5. find the candidate with the most amount of votes
+// 6. choose candidate with highest number of votes and record as winner
+
+// __________________________________________
+// Initial Solution
+
+// for (var name in votes){
+//   for(var position in votes[name]){
+//     var candidate = votes[name][position]
+//     voteCount[position][candidate] = (voteCount[position][candidate] || 0)+1;
+//   }
+// }
+
+
+// for(var position in voteCount){
+//   var counter = 0;
+//   for(var candidate in voteCount[position]){
+//     var votes = voteCount[position][candidate];
+//         if (votes > counter) {
+//           counter = votes;
+//           officers[position] = candidate;
+//         }
+//     }
+// }
+
+//console.log(voteCount);
+
+
+// __________________________________________
+// Refactored Solution
+
+for (var name in votes){
+  for(var position in votes[name]){
+    var candidate = votes[name][position]
+      voteCount[position][candidate] = (voteCount[position][candidate] || 0)+1;
+      }
+  for(var position in voteCount){
+     var counter = 0;
+      for(var candidate in voteCount[position]){
+        var votes = voteCount[position][candidate];
+          if (votes > counter) {
+            counter = votes;
+            officers[position] = candidate;
+            }
+          }
+        }
+      };
+
+// __________________________________________
+// Reflection
+/*
+What did you learn about iterating over nested objects in JavaScript?
+  We learned you have to go through each nested object in "layers."
+Were you able to find useful methods to help you with this?
+  The most useful thing we found was the if statement that uses a counter variable to pull the candidate with the highest number of votes.
+What concepts were solidified in the process of working through this challenge?
+  We originally, tried to use the math.max method to choose the candidate with the highest number of votes, but then found an easier, dryer way of doing that. By doing that, we solidified how to run a counter variable and the specific placement of that variable.
+*/
+
+
+// __________________________________________
+// Test Code:  Do not alter code below this line.
+
+
+function assert(test, message, test_number) {
+  if (!test) {
+    console.log(test_number + "false");
+    throw "ERROR: " + message;
+  }
+  console.log(test_number + "true");
+  return true;
+}
+
+assert(
+  (voteCount.president["Bob"] === 3),
+  "Bob should receive three votes for President.",
+  "1. "
+)
+
+assert(
+  (voteCount.vicePresident["Bob"] === 2),
+  "Bob should receive two votes for Vice President.",
+  "2. "
+)
+
+assert(
+  (voteCount.secretary["Bob"] === 2),
+  "Bob should receive two votes for Secretary.",
+  "3. "
+)
+
+assert(
+  (voteCount.treasurer["Bob"] === 4),
+  "Bob should receive four votes for Treasurer.",
+  "4. "
+)
+
+assert(
+  (officers.president === "Louise"),
+  "Louise should be elected President.",
+  "5. "
+)
+
+assert(
+  (officers.vicePresident === "Hermann"),
+  "Hermann should be elected Vice President.",
+  "6. "
+)
+
+assert(
+  (officers.secretary === "Fred"),
+  "Fred should be elected Secretary.",
+  "7. "
+)
+
+assert(
+  (officers.treasurer === "Ivy"),
+  "Ivy should be elected Treasurer.",
+  "8. "
+)
